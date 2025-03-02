d4048deb6df36e09af6bc4f8ff73bef34208a897
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-03-02 10:24:04 -0500

    Cree el modelo cliente

---
+++ BackendSocrates/src/main/java/com/example/BackendSocrates/model/Cliente.java
@@ -0,0 +1,52 @@
+package com.example.BackendSocrates.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity(name = "clientes")
+public class Cliente {   //FALTA PONER EL EXTENDS DE PERSONA
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "tipo_servicio")
+    private String tipoServicio;
+
+    @Column(name = "fecha_registro")
+    private Date fechaRegistro;
+
+    public Cliente() {
+    }
+
+    public Cliente(long id, String tipoServicio, Date fechaRegistro) {
+        this.id = id;
+        this.tipoServicio = tipoServicio;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTipoServicio() {
+        return tipoServicio;
+    }
+
+    public void setTipoServicio(String tipoServicio) {
+        this.tipoServicio = tipoServicio;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+}
