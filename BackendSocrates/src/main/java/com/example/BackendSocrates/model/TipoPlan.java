e1946c6c0b0549980ee9454a2a129db5fb8fc946
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-03-02 10:57:00 -0500

    Cree el modelo de tipoPlan

---
+++ BackendSocrates/src/main/java/com/example/BackendSocrates/model/TipoPlan.java
@@ -0,0 +1,74 @@
+package com.example.BackendSocrates.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "tipos_planes")
+public class TipoPlan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "precio")
+    private double precio;
+
+    @Column(name = "estado")
+    private boolean estado;
+
+    public TipoPlan() {
+    }
+
+    public TipoPlan(long id, String nombre, String descripcion, double precio, boolean estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.estado = estado;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+}
