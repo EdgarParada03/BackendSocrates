6c7eda023f3d828eb8c476cb9311c38798f3d73f
Edgar Yamill Parada Duarte <104601278+EdgarParada03@users.noreply.github.com>
2025-03-01 21:16:12 -0500

    Cargue el archivo base de spring boot

---
+++ BackendSocrates/src/main/java/com/example/BackendSocrates/BackendSocratesApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackendSocrates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSocratesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSocratesApplication.class, args);
+	}
+
+}
---
+++ BackendSocrates/src/test/java/com/example/BackendSocrates/BackendSocratesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendSocrates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSocratesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
