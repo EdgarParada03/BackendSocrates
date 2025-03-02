3ffba221a083aa00849d8e417947b6ee54d5be6b
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-03-02 10:57:39 -0500

    Cree el repositorio de tipoPlan

---
+++ BackendSocrates/src/main/java/com/example/BackendSocrates/repositories/TipoPlanRepository.java
@@ -0,0 +1,7 @@
+package com.example.BackendSocrates.repositories;
+
+import com.example.BackendSocrates.model.TipoPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoPlanRepository extends JpaRepository<TipoPlan, Long> {
+}
