eada22e4c6db2c7734efec147ffe078eb1b30b0e
jose sandoval <luisvalr120@gmail.com>
2025-03-04 14:55:17 -0500

    Agregue modelo, repositorio y controlador de ADMINISTRADOR

---
+++ BackendSocrates/src/main/java/com/example/BackendSocrates/repositories/AdministradorRepository.java
@@ -0,0 +1,7 @@
+package com.example.BackendSocrates.repositories;
+
+import com.example.BackendSocrates.model.Administrador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
+}
