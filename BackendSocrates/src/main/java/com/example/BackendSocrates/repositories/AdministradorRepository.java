553c0d96d3889af71c1afac7b81f5086ac000fd7
jose sandoval <luisvalr120@gmail.com>
2025-03-15 10:27:06 -0500

    Elimine Administrador

--- BackendSocrates/src/main/java/com/example/BackendSocrates/repositories/AdministradorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BackendSocrates.repositories;
-
-import com.example.BackendSocrates.model.Administrador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
-}
