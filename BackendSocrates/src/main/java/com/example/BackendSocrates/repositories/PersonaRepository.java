e2f4e2315af5069cf684c5b7fb81a5438b1db0c2
CRISTIAN <cristianestrada1609@gmail.com>
2025-03-15 08:47:01 -0500

    Cargo el modelo, controlador, y prueba uniartaria para persona

---
+++ BackendSocrates/src/main/java/com/example/BackendSocrates/repositories/PersonaRepository.java
@@ -0,0 +1,9 @@
+package com.example.BackendSocrates.repositories;
+
+import com.example.BackendSocrates.model.Administrador;
+import com.example.BackendSocrates.model.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PersonaRepository extends JpaRepository<Persona, Long> {
+}
