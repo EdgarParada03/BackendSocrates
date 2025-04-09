ba94b21a5e8236874f014f527a555e0d146ffa91
jose sandoval <luisvalr120@gmail.com>
2025-04-08 22:53:08 -0500

    controlador de login

---
+++ BackendSocrates/src/main/java/com/example/BackendSocrates/controllers/AuthController.java
@@ -0,0 +1,21 @@
+package com.example.BackendSocrates.controllers;
+
+import com.example.BackendSocrates.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = {"*"})
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestParam String numeroDocumento, @RequestParam String password,  @RequestParam String cargo) {
+        return authService.authenticate(numeroDocumento, password,cargo);
+    }
+}
